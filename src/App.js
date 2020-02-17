6946f0468e05eb3792e401f1659d7302c37a3ddc
Hemin Johnson <achayan1992@gmail.com>
2020-02-17 14:46:10 +0100

    feat: add the rank component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./App.css";
 import "tachyons";
 
-import Navigation from "./components/Navigation/Navigation";
+import Navigation from "./components/Navigation/navigation";
 import Logo from "./components/Logo/logo";
 import ImageLinkForm from "./components/ImageLinkForm/imageLinkForm";
+import Rank from "./components/Rank/rank";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
+      <Rank />
       <ImageLinkForm />
     </div>
   );
