b7a2102496d1a3b537e9ce0c8a05e2d6d33a790b
Hemin Johnson <achayan1992@gmail.com>
2020-02-17 15:13:51 +0100

    feat: add particles component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./App.css";
 import "tachyons";
+import Particles from "react-particles-js";
 
 import Navigation from "./components/Navigation/navigation";
 import Logo from "./components/Logo/logo";
 import ImageLinkForm from "./components/ImageLinkForm/imageLinkForm";
 import Rank from "./components/Rank/rank";
 
+const particleOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 1000
+      }
+    }
+  }
+};
+
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particleOptions} />
       <Navigation />
       <Logo />
       <Rank />
