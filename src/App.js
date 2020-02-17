67a35e842c4e07edba64df17ad761de0b22ef8be
Hemin Johnson <achayan1992@gmail.com>
2020-02-17 13:35:29 +0100

    feat: add logo component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 import "tachyons";
 
 import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/logo";
 
 function App() {
   return (
     <div className="App">
       <Navigation />
+      <Logo />
     </div>
   );
 }
