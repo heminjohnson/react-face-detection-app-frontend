8706a2742b4b42d697acf87525bf7edb65bd8560
Hemin Johnson <achayan1992@gmail.com>
2020-02-18 22:22:07 +0100

    feat: connect clarifai to the app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "tachyons";
+
 import Particles from "react-particles-js";
+import Clarifai from "clarifai";
 
 import Navigation from "./components/Navigation/navigation";
 import Logo from "./components/Logo/logo";
 import ImageLinkForm from "./components/ImageLinkForm/imageLinkForm";
 import Rank from "./components/Rank/rank";
+import FaceRecognition from "./components/FaceRecognition/faceRecognition";
 
 const particleOptions = {
   particles: {
@@ -20,14 +23,43 @@ const particleOptions = {
   }
 };
 
+const app = new Clarifai.App({
+  apiKey: process.env.REACT_APP_CLARIFY_API_KEY
+});
+
 function App() {
+  const [input, setInput] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const onInputChange = event => {
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setImageUrl(input);
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, input).then(
+      response => {
+        console.log(
+          response.outputs[0].data.regions[0].region_info.bounding_box
+        );
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  };
+
   return (
     <div className="App">
       <Particles className="particles" params={particleOptions} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
+      {imageUrl && <FaceRecognition imageUrl={imageUrl} />}
     </div>
   );
 }
